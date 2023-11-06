873a76f00a81254669c660f95fa3b9456c4adf48
Ryan Sousa <ryan.sousa4000@gmail.com>
2023-11-06 14:25:32 -0300

    First commit

---
+++ src/main/java/com/compassuol/sp/challenge03/Challenge03Application.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/compassuol/sp/challenge03/Challenge03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
